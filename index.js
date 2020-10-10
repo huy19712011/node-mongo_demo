b2c251dc220a5bfdca3158a41e3b669bf756ce73
Huy Nguyen Quang <huy19712011@gmail.com>
2020-10-10 09:33:40 +0700

    end of 5 Querying documents == begin of 6 more complex query operators

--- index.js
+++ index.js
@@ -36,4 +36,14 @@ async function createCourse() {
   console.log(result);
 }
 
-createCourse();
+async function getCourses(){
+  const courses = await Course
+    .find({author: "Mosh", isPublished: true})
+    .limit(10)
+    .sort({name: -1}) // 1 = ASC, -1 = DESC
+    .select({name: 1, tags: 1}); 
+  console.log(courses);
+}
+
+// createCourse();
+getCourses();
